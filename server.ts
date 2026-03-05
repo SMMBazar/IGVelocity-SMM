70aec1e8c0b363c8aefac9cfb34785873a0d17fe
SMMBazar <himanshusah465@gmail.com>
2026-03-05 13:45:57 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -601,3 +601,12 @@ async function startServer() {
 }
 
 startServer();
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
